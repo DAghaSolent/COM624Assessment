915774b050c2f1133526358af8013b7ee382b219
DAghaSolent <5aghad30@solent.ac.uk>
2023-11-27 00:27:51 +0000

    Adjustments made to complete task 1 to successfully.

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ import yfinance as yf
 from datetime import datetime, timedelta
 import pandas as pd
 
-# Creating variables to be used to set dates to download data within a 1 year timeframe.
+# Nasdaq 100 companies stored in a list below called tickers
+tickers = ['AAPL', 'MSFT', 'AMZN', 'NVDA', 'META', 'AVGO', 'GOOGL', 'GOOG', 'TSLA', 'ADBE', 'COST', 'PEP', 'NFLX', 'AMD'
+           , 'CSCO', 'INTC', 'TMUS', 'CMCSA', 'INTU', 'QCOM', 'AMGN', 'TXN', 'HON', 'AMAT', 'SBUX', 'ISRG', 'BKNG',
+           'MDLZ', 'LRCX', 'ADP', 'GILD', 'ADI', 'VRTX', 'REGN', 'MU', 'SNPS', 'PANW', 'PDD', 'MELI', 'KLAC', 'CDNS',
+           'CSX', 'MAR', 'PYPL', 'CHTR', 'ASML', 'ORLY', 'MNST', 'CTAS', 'ABNB', 'LULU', 'NXPI', 'WDAY', 'CPRT', 'MRVL',
+           'PCAR', 'CRWD', 'KDP', 'MCHP', 'ROST', 'ODFL', 'DXCM', 'ADSK', 'KHC', 'PAYX', 'FTNT', 'AEP', 'SGEN', 'CEG',
+           'IDXX', 'EXC', 'AZN', 'EA', 'CTSH', 'FAST', 'VRSK', 'CSGP', 'BKR', 'DDOG', 'BIIB', 'GEHC', 'XEL', 'GFS',
+           'TTD', 'ON', 'MRNA', 'ZS', 'TEAM', 'FANG', 'WBD', 'ANSS', 'DLTR', 'EBAY', 'SIRI', 'WBA', 'ALGN', 'ZM', 'ILMN'
+           , 'ENPH', 'JD', 'LCID']
+
+# Creating variables to be used to set dates to download data within a 1-year timeframe.
 end_date = datetime.today()
 start_date = end_date - timedelta(365)
 
-# Download 1 year timeframe financial data for Nasdaq 100 index
-data = yf.download("^NDX", start=start_date, end=end_date)
+# Empty dataframe which will be used to store the Adjusted close values for each Nasdaq 100 company that is stored in
+# the tickers list.
+adjClose_data = pd.DataFrame()
 
-# Utilising Pandas to display more rows
-pd.set_option('display.max_rows', None)
+# Loop through each ticker within the tickers list and download 1 year adjusted close prices for that individual ticker,
+# once obtained put that information in the new data frame that I created above
+for ticker in tickers:
+    data = yf.download(ticker, start=start_date, end=end_date)
+    adjClose_data[ticker] = data['Adj Close']
 
-print(data)
+# Utilising Pandas to display more columns
+pd.set_option('display.max_columns', None)
 
-
-
+print(adjClose_data)
